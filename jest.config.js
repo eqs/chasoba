395c17ac38ac1375e33bd5c06408b77d669d7bc3
eqs <murashige.satoshi.mi1@gmail.com>
2023-12-25 00:12:54 +0900

    Add example test with jest

---
+++ jest.config.js
@@ -0,0 +1,46 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+/** @type {import('jest').Config} */
+const config = {
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  moduleNameMapper: pathsToModuleNameMapper(
+    compilerOptions.paths, {
+      prefix: compilerOptions.rootDir + '/'
+    }
+  ),
+
+  // The root directory that Jest should scan for tests and modules within
+  rootDir: './tests',
+
+  // A list of paths to directories that Jest should use to search for files in
+  roots: [
+    "<rootDir>/"
+  ],
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: [
+    "\\\\node_modules\\\\"
+  ],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+};
+
+module.exports = config;
---
+++ tests/example.test.ts
@@ -0,0 +1,5 @@
+import { show } from '@/utils';
+
+test('test', () => {
+  expect(21 + 21).toBe(42);
+});
