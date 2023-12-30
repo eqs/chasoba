0821704a5b102d206f80185da48fedec47e90d04
eqs <murashige.satoshi.mi1@gmail.com>
2023-12-30 14:43:14 +0900

    Split cli from index

---
+++ src/cli.ts
@@ -0,0 +1,26 @@
+import { read, show } from './utils';
+import { Chasoba } from './index';
+
+
+function main() {
+  const args = process.argv.slice(2);
+
+  if (args.length == 0) {
+    console.log('No input files.');
+    process.exit(1);
+  }
+
+  let verbose = false;
+  if (args.length >= 1 && args[1] == '--verbose') {
+    verbose = true;
+  }
+
+  const source = read(args[0]);
+
+  const chasoba = new Chasoba();
+  const svg = chasoba.render(source, verbose);
+
+  console.log(svg);
+}
+
+main();
--- src/index.ts
+++ src/index.ts
@@ -40,26 +40,3 @@ export class Chasoba {
     return svg;
   }
 }
-
-function main() {
-  const args = process.argv.slice(2);
-
-  if (args.length == 0) {
-    console.log('No input files.');
-    process.exit(1);
-  }
-
-  let verbose = false;
-  if (args.length >= 1 && args[1] == '--verbose') {
-    verbose = true;
-  }
-
-  const source = read(args[0]);
-
-  const chasoba = new Chasoba();
-  const svg = chasoba.render(source, verbose);
-
-  console.log(svg);
-}
-
-main();
